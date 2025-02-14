91ffc882297d9930b9d562ef25d1a03ea1c1e758
dan9qq7 <75699450+dan9qq7@users.noreply.github.com>
2025-02-14 15:10:38 +0000

    done

--- script.js
+++ script.js
@@ -6,5 +6,22 @@ alert("My favorite color is " + color);
 var potatochips = prompt("favorite color is what?")
 alert("your favorite color is " + potatochips + "!")
 
+/* Var Alert */
+// make an integer variable for age, set it equal to your age
+// make a string variable for name, set it to your name
+
+// alert name is age years old
 
 
+/* Var Alert Prompt */
+// make an integer variable for age, prompt user for their age
+var age = prompt("what is your age?")
+// make a string variable for name, prompt user for their name
+// alert name is age years old
+var name1 = prompt("whats your name?") ;
+alert(name1 + " is " + age + " years old");
+/* Var Alert Prompt Conditional */
+// make an integer variable for age, prompt user for it
+// make a string variable for name, prompt user for it
+// if age is greater than 16, alert "you don't look that old!"
+// otherwise alert name is age years old
