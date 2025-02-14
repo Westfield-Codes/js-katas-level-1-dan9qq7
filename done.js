0c3187a0bb76892eb9c7f8e774624d6cb2f1b96e
dan9qq7 <75699450+dan9qq7@users.noreply.github.com>
2025-02-14 15:22:18 +0000

    color

--- done.js
+++ done.js
@@ -6,6 +6,11 @@
 var color = "black";
 alert("My favorite color is " + color);
 /* Prompt */
+/* Var Alert Prompt */
+// make a string variable for color, but prompt the user for their favorite color.
+var color = prompt("favorite color?");
+// alert color plus " is a nice color!" - note the space in the quotes.
+alert("your favorite color is " + color + "!");
 
 /* Prompt with Conditional */
 
--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-/* Var Alert */
-// make a string variable named color, set it equal to your favorite color.
-var color = "black";
-// alert the message" "My favorite color is " plus the variable name, outside the quotes.
-alert("My favorite color is " + color);
-var potatochips = prompt("favorite color is what?")
-alert("your favorite color is " + potatochips + "!")
-
-/* Var Alert */
-// make an integer variable for age, set it equal to your age
-// make a string variable for name, set it to your name
-
-// alert name is age years old
-
 
 /* Var Alert Prompt */
-// make an integer variable for age, prompt user for their age
-var age = prompt("what is your age?")
-// make a string variable for name, prompt user for their name
-// alert name is age years old
-var name1 = prompt("whats your name?") ;
-alert(name1 + " is " + age + " years old");
-/* Var Alert Prompt Conditional */
-// make an integer variable for age, prompt user for it
-// make a string variable for name, prompt user for it
-// if age is greater than 16, alert "you don't look that old!"
-// otherwise alert name is age years old
+// make a string variable for color, but prompt the user for their favorite color.
+var color = prompt("favorite color?");
+// alert color plus " is a nice color!" - note the space in the quotes.
+alert("your favorite color is " + color + "!");
+
