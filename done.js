407b54d438f9eacc8c7a28c7019ecb123db09c81
dan9qq7 <75699450+dan9qq7@users.noreply.github.com>
2025-02-14 15:31:37 +0000

    ice cream

--- done.js
+++ done.js
@@ -29,6 +29,14 @@ alert(name1 + " is " + age + " years old");
 
 /* ICE CREAM */
 
+/* Var Alert Prompt */
+// make a string variable for favorite ice cream flavor, prompt user for it
+var favice = prompt("what is your favorite ice cream flavor?");
+// make an integer variable for number of scoops on the cone, prompt user for it
+var scoops = prompt("number of scoops of that flavor that you prefer?");
+// alert "You want " scoops "scoops of " flavor
+alert("You want " + scoops + " scoops of " + favice);
+
 /* No Prompt */
 
 
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
 /* Var Alert Prompt */
-// make a string variable for color, but prompt the user for their favorite color.
-var color = prompt("favorite color?");
-// alert color plus " is a nice color!" - note the space in the quotes.
-alert("your favorite color is " + color + "!");
-
+// make a string variable for favorite ice cream flavor, prompt user for it
+var favice = prompt("what is your favorite ice cream flavor?");
+// make an integer variable for number of scoops on the cone, prompt user for it
+var scoops = prompt("number of scoops of that flavor that you prefer?");
+// alert "You want " scoops "scoops of " flavor
+alert("You want " + scoops + " scoops of " + favice);
