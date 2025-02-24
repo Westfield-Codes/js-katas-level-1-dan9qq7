69191d5c1fa4c26744e651ef8ee6ae118d46aa0d
dan9qq7 <75699450+dan9qq7@users.noreply.github.com>
2025-02-24 15:27:04 +0000

    done

--- done.js
+++ done.js
@@ -49,7 +49,13 @@ alert("You want " + scoops + " scoops of " + favice);
 /* PET */
 
 /* No Prompt */
-
+/* Var Alert Prompt */
+// make a variable for a pet type, prompt user for it 
+var pt = prompt ("What is your pet?");
+// make a variable for a pet name, prompt user for it
+var pn = prompt ("what is your pets name?");
+// alert "You have a pet type named pet name 
+alert("your pet is " + pt + " " + pn + "!");
 
 /* Prompt */
 
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* Var Alert Prompt */
-// make a string variable for favorite ice cream flavor, prompt user for it
-var favice = prompt("what is your favorite ice cream flavor?");
-// make an integer variable for number of scoops on the cone, prompt user for it
-var scoops = prompt("number of scoops of that flavor that you prefer?");
-// alert "You want " scoops "scoops of " flavor
-alert("You want " + scoops + " scoops of " + favice);
+// make a variable for a pet type, prompt user for it 
+var pt = prompt ("What is your pet?");
+// make a variable for a pet name, prompt user for it
+var pn = prompt ("what is your pets name?");
+// alert "You have a pet type named pet name 
+alert("your pet is " + pt + " " + pn + "!");
